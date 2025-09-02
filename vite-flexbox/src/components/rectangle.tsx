8e2dfb80ee05de21c28b925316564adcac65d28d
ntorh <ngcobontombenhle434@gmail.com>
2025-09-02 13:09:59 +0200

    did some changes on rectangle.tsx file

--- vite-flexbox/src/components/rectangle.tsx
+++ vite-flexbox/src/components/rectangle.tsx
@@ -4,26 +4,34 @@ import React from "react";
 export default function FlexboxChallenge() {
   return (
     <div className="container">
-      <div className="rowSpan2">
-        <div className="calSpan1">
-          <div className="orangeRectangle"></div>
+     
+      <div className="firsRect">
+        <div className="orangeRectangle">
+      
+        <div className="column1">
+          <div className="circleRed"></div>
+          <div className="ligtBlueRectangle"></div>
         </div>
-        <div className="calSpan4">
-          <div className="row"></div>
-          <div className="row"></div>
-        </div>
-      </div>
-      <div className="first4">
-        <div className="orangeRectangle"></div>
 
-        <div className="circleRed"></div>
-        <div className="squareRed"></div>
-        <div className="BlueRect"></div>
+    <div className="column2">
+    <div className="squareRed"></div>
+    <div className="Redcircle"></div>
+    </div>
+
+    <div className="column3">
+     <div className="BlueRect"></div>
+     <div className="ligtBlueCicle"></div>
+    </div>
+
+
+       
+      
+       
       </div>
       <div className="sec3">
-        <div className="ligtBlueRectangle"></div>
-        <div className="Redcircle"></div>
-        <div className="ligtBlueCicle"></div>
+       
+    
+    
       </div>
       <div className="third3">
         <div className="BlueRectangle"></div>
